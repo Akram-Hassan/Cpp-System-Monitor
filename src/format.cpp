6d369f101c7a24251cbab2455370b1315aba9289
Akram Hassan <akram.imam82@gmail.com>
2023-11-05 14:26:42 +0100

    remove [[maybe_unused]]

--- src/format.cpp
+++ src/format.cpp
@@ -8,6 +8,6 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { 
+string Format::ElapsedTime(long seconds) { 
     return "this is test string!"; 
 }
