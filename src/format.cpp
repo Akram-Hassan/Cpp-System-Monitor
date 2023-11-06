4130e515ddec0c07dcfcdbc75a759560f326546d
Akram Hassan <akram.imam82@gmail.com>
2023-11-06 03:17:16 +0100

    Implement Format::ElapsedTime

--- src/format.cpp
+++ src/format.cpp
@@ -8,6 +8,15 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds) { 
-    return "this is test string!"; 
+string Format::ElapsedTime(long seconds) {
+    long hours = seconds / 3600;
+    long seconds_after_hours = seconds % 3600;
+
+    long minutes = seconds_after_hours / 60;
+    long seconds_result = seconds_after_hours % 60;
+
+    char result[8];
+    sprintf(result, "%02d:%02d:%02d", hours, minutes, seconds_result);
+
+    return string(result); 
 }
--- tests/format_tests.cpp
+++ tests/format_tests.cpp
@@ -4,5 +4,16 @@
 #include <gtest/gtest.h>
 
 TEST(FormatTest, BasicAssertions) {
-  EXPECT_STREQ(Format::ElapsedTime(20).c_str(), "this is test string!");
+  EXPECT_STREQ(Format::ElapsedTime(20).c_str(), "00:00:20");
+  EXPECT_STREQ(Format::ElapsedTime(59).c_str(), "00:00:59");
+  EXPECT_STREQ(Format::ElapsedTime(60).c_str(), "00:01:00");
+  EXPECT_STREQ(Format::ElapsedTime(65).c_str(), "00:01:05");
+
+  EXPECT_STREQ(Format::ElapsedTime(3540).c_str(), "00:59:00");
+  EXPECT_STREQ(Format::ElapsedTime(3600).c_str(), "01:00:00");
+  EXPECT_STREQ(Format::ElapsedTime(3601).c_str(), "01:00:01");
+  EXPECT_STREQ(Format::ElapsedTime(3724).c_str(), "01:02:04");
+  
+  EXPECT_STREQ(Format::ElapsedTime(10803).c_str(), "03:00:03");
+  EXPECT_STREQ(Format::ElapsedTime(13627).c_str(), "03:47:07");
 }
